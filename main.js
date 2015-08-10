03a1f82b88d76d00210194d99005281e458f4ad9
Peterson Trethewey <peterson.trethewey@gmail.com>
2015-08-10 15:16:54 -0700

    Adding main

---
+++ main.js
@@ -0,0 +1,32 @@
+
+TextEditViewApp = function()
+{
+    this.textView = null;
+}
+
+TextEditViewApp.makeTextView = function(idName, className)
+{
+    var elem = document.createElement('div');
+    elem.id = idName;
+    elem.class = className;
+    return elem;
+}
+
+TextEditViewApp.prototype.launch = function(containerDiv)
+{
+    containerDiv.style.backgroundColor = "#eee";
+    this.textView = makeTextView("my-text-view-div", "fill")
+    containerDiv.appendChild( this.textView );
+}
+
+TextEditViewApp.prototype.open = function(fileAccess)
+{
+    var thisApp = this;
+    fileAccess.download(
+        function(text)
+        {
+            // assign text to view here
+        }
+    );
+}
+
