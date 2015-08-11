da13c352a32cc89b959d98ef936a64e28077e183
Peterson Trethewey <peterson.trethewey@gmail.com>
2015-08-10 23:55:46 -0700

    Put main inside anonymous empty-argument function returing the class.

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
 
-TextEditViewApp = function()
+function()
 {
-    this.textView = null;
-}
+    TextEditViewApp = function()
+    {
+        this.textView = null;
+    }
 
-TextEditViewApp.makeTextView = function(idName, className)
-{
-    var elem = document.createElement('div');
-    elem.id = idName;
-    elem.class = className;
-    return elem;
-}
+    TextEditViewApp.makeTextView = function(idName, className)
+    {
+        var elem = document.createElement('div');
+        elem.id = idName;
+        elem.class = className;
+        return elem;
+    }
 
-TextEditViewApp.prototype.launch = function(containerDiv)
-{
-    containerDiv.style.backgroundColor = "#eee";
-    this.textView = makeTextView("my-text-view-div", "fill")
-    containerDiv.appendChild( this.textView );
-}
+    TextEditViewApp.prototype.launch = function(containerDiv)
+    {
+        containerDiv.style.backgroundColor = "#eee";
+        this.textView = makeTextView("my-text-view-div", "fill")
+        containerDiv.appendChild( this.textView );
+    }
 
-TextEditViewApp.prototype.open = function(fileAccess)
-{
-    var thisApp = this;
-    fileAccess.download(
-        function(text)
-        {
-            // assign text to view here
-        }
-    );
+    TextEditViewApp.prototype.open = function(fileAccess)
+    {
+        var thisApp = this;
+        fileAccess.download(
+            function(text)
+            {
+                // assign text to view here
+            }
+        );
+    }
+
+    return TextEditViewApp;
 }
 
